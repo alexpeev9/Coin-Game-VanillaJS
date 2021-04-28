7f135f91bff8f303ba6ebd3ce1ea5d304fc6b790
alexpeev9 <alexpeev9@abv.bg>
2021-04-28 23:22:56 +0300

    Added Animation for coin toss and new pictures

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,22 @@
-function flip()
-{
-    console.log('Heads');
+const coin = document.querySelector('#coin');
+const button = document.querySelector('#flip');
+
+function deferFn(callback, ms) {
+    setTimeout(callback, ms);
 }
+
+function processResult(result) {
+    alert(result);
+}
+
+function flipCoin() {
+    const result = Math.random() < 0.5 ? 'head' : 'tail';
+    deferFn(function () {
+        coin.setAttribute('class', 'animate-' + result);
+        deferFn(processResult.bind(null, result), 2900);
+    }, 100);
+}
+
+button.addEventListener('click', flipCoin);
+
+
